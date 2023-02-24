0b7fdf04f7c8c528fdc89c3cdcaa74aaea159154
Enrique Lopez <ingeniero.enriquelopez@gmail.com>
2023-02-24 15:40:32 -0600

    44444

--- src/App.js
+++ src/App.js
@@ -11,6 +11,12 @@ function App() {
       <header className="App-header">
         <h1>LOLO</h1>
         <Home/>
+        <hr/>
+        <ol>
+          <li>SQL SERVER</li>
+          <li>POSTGRESQL</li>
+          <li>MONGODB</li>
+        </ol>
       </header>
     </div>
   );
