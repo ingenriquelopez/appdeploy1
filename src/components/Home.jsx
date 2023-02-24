9783e117511ccd3cbf983243a630f4f7c5ba40c4
Enrique Lopez <ingeniero.enriquelopez@gmail.com>
2023-02-24 15:28:27 -0600

    tercero

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+         <h2> ESTOY EN HOME</h2>
+         <hr/>
+    </div>
+   
+  )
+}
+<h2> ESTOY EN HOME</h2>
+
+export default Home
